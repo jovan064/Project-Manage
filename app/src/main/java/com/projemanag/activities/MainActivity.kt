a0d5fd22841cd4425c4d20351d8337517adb2ab3
jovan064 <119740714+jovan064@users.noreply.github.com>
2023-01-23 01:13:03 +0100

    Commit

---
+++ app/src/main/java/com/projemanag/activities/MainActivity.kt
@@ -0,0 +1,207 @@
+package com.projemanag.activities
+
+import CreateBoardActivity
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.projemanag.R
+import com.projemanag.adapters.BoardItemsAdapter
+import com.projemanag.firebase.FirestoreClass
+import com.projemanag.model.Board
+import com.projemanag.model.User
+import com.projemanag.utils.Constants
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    // A global variable for User Name
+    private lateinit var mUserName: String
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_main)
+
+        setupActionBar()
+
+        // Assign the NavigationView.OnNavigationItemSelectedListener to navigation view.
+        nav_view.setNavigationItemSelectedListener(this)
+
+        // TODO (Step 6: Here pass the parameter value as TRUE to read the boards rest all are FALSE.)
+        // START
+        // Get the current logged in user details.
+        // Show the progress dialog.
+        showProgressDialog(resources.getString(R.string.please_wait),FirestoreClass().loadUserData(this@MainActivity, true))
+
+        // END
+
+        fab_create_board.setOnClickListener {
+            val intent = Intent(this@MainActivity, CreateBoardActivity::class.java)
+            intent.putExtra(Constants.NAME, mUserName)
+            startActivity(intent)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            // A double back press function is added in Base Activity.
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_my_profile -> {
+
+                startActivityForResult(
+                    Intent(this@MainActivity, MyProfileActivity::class.java),
+                    MY_PROFILE_REQUEST_CODE
+                )
+            }
+
+            R.id.nav_sign_out -> {
+                // Here sign outs the user from firebase in this device.
+                FirebaseAuth.getInstance().signOut()
+
+                // Send the user to the intro screen of the application.
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == MY_PROFILE_REQUEST_CODE
+        ) {
+            // Get the user updated details.
+            FirestoreClass().loadUserData(this@MainActivity)
+        } else {
+            Log.e("Cancelled", "Cancelled")
+        }
+    }
+
+    /**
+     * A function to setup action bar
+     */
+    private fun setupActionBar() {
+
+        setSupportActionBar(toolbar_main_activity)
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolbar_main_activity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    /**
+     * A function for opening and closing the Navigation Drawer.
+     */
+    private fun toggleDrawer() {
+
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    // TODO (Step 7: Add a parameter to check whether to read the boards list or not.)
+    /**
+     * A function to get the current user details from firebase.
+     */
+    fun updateNavigationUserDetails(user: User, isToReadBoardsList: Boolean) {
+
+        hideProgressDialog()
+
+        mUserName = user.name
+
+        // The instance of the header view of the navigation view.
+        val headerView = nav_view.getHeaderView(0)
+
+        // The instance of the user image of the navigation view.
+        val navUserImage = headerView.findViewById<ImageView>(R.id.iv_user_image)
+
+        // Load the user image in the ImageView.
+        Glide
+            .with(this@MainActivity)
+            .load(user.image) // URL of the image
+            .centerCrop() // Scale type of the image.
+            .placeholder(R.drawable.ic_user_place_holder) // A default place holder
+            .into(navUserImage) // the view in which the image will be loaded.
+
+        // The instance of the user name TextView of the navigation view.
+        val navUsername = headerView.findViewById<TextView>(R.id.tv_username)
+        // Set the user name
+        navUsername.text = user.name
+
+        // TODO (Step 8: Here if the isToReadBoardList is TRUE then get the list of boards.)
+        // START
+        if (isToReadBoardsList) {
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait),FirestoreClass().getBoardsList(this@MainActivity))
+
+        }
+        // END
+    }
+
+    // TODO (Step 1: Create a function to populate the result of BOARDS list in the UI i.e in the recyclerView.)
+    // START
+    /**
+     * A function to populate the result of BOARDS list in the UI i.e in the recyclerView.
+     */
+    fun populateBoardsListToUI(boardsList: ArrayList<Board>) {
+
+        hideProgressDialog()
+
+        if (boardsList.size > 0) {
+
+            rv_boards_list.visibility = View.VISIBLE
+            tv_no_boards_available.visibility = View.GONE
+
+            rv_boards_list.layoutManager = LinearLayoutManager(this@MainActivity)
+            rv_boards_list.setHasFixedSize(true)
+
+            // Create an instance of BoardItemsAdapter and pass the boardList to it.
+            val adapter = BoardItemsAdapter(this@MainActivity, boardsList)
+            rv_boards_list.adapter = adapter // Attach the adapter to the recyclerView.
+        } else {
+            rv_boards_list.visibility = View.GONE
+            tv_no_boards_available.visibility = View.VISIBLE
+        }
+    }
+    // END
+
+    /**
+     * A companion object to declare the constants.
+     */
+    companion object {
+        //A unique code for starting the activity for result
+        const val MY_PROFILE_REQUEST_CODE: Int = 11
+    }
+}
---
+++ app/src/main/java/com/projemanag/utils/Constants.kt
@@ -0,0 +1,33 @@
+package com.projemanag.utils
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import android.webkit.MimeTypeMap
+import com.projemanag.activities.MyProfileActivity
+import com.projemanag.utils.Constants.PICK_IMAGE_REQUEST_CODE
+
+object Constants {
+
+     const val READ_STORAGE_PERMISSION_CODE = 1
+     const val PICK_IMAGE_REQUEST_CODE = 2
+     const val USERS: String = "users"
+     const val IMAGE: String = "image"
+     const val NAME: String = "name"
+     const val MOBILE: String = "mobile"
+     const val BOARDS: String = "boards"
+     const val ASSIGNEDTO: String = "assigned to"
+
+     fun showImageChooser(activity: Activity) {
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+       activity.startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+     fun getFileExtension(activity: Activity, uri: Uri?): String? {
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(activity.contentResolver.getType(uri!!))
+    }
+}
